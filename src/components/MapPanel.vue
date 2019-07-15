189f2240c78a9bdcc348f0bf507535ed8f3d965f
stevetotheizz0 <stephen.skilton@phila.gov>
2019-07-15 16:49:22 -0400

    Removed unused class from css

--- src/components/MapPanel.vue
+++ src/components/MapPanel.vue
@@ -758,7 +758,7 @@
       top: 29%;
     }
 
-    .leaflet-draw-polygon, .leaflet-draw-toolbar.leaflet-bar.leaflet-draw-toolbar-top {
+    .leaflet-draw-toolbar.leaflet-bar.leaflet-draw-toolbar-top {
       width: 34px !important;
       height: 34px !important;
       background: white;
@@ -766,7 +766,7 @@
     }
 
     a.leaflet-draw-draw-polygon {
-      width: 30px !important;
+      width: 30px ;
       height: 30px !important;
       background-position: -65px -10px !important;
       background-size: 450px 48px !important;
