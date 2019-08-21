46b3d66562ffc53307a64c2bd7a8f4a62039e131
andy.rothwell <rothwell.andy@gmail.com>
2019-08-21 10:33:58 -0400

    completes changes in cleaning up property card modal

--- src/store.js
+++ src/store.js
@@ -40,6 +40,7 @@ function createStore(config) {
       featureId: null,
       tableId: null
     },
+    activeModalFeature: null,
     activeModal: {
       featureId: null,
     },
@@ -137,20 +138,10 @@ function createStore(config) {
         state.map.map = payload.map;
       },
       setMapBounds(state, payload) {
-        // const { northEast, southWest } = payload || {};
-        // state.map.bounds.northEast = northEast;
-        // state.map.bounds.southWest = southWest;
         state.map.bounds = payload;
       },
       setMapBoundsBasedOnShape(state, payload) {
         state.map.boundsBasedOnShape = payload
-      },
-      setActiveParcel(state, payload) {
-        // console.log('store setActiveParcel:', payload)
-        const { parcelLayer, activeParcel, activeAddress, activeMapreg } = payload || {};
-        state.parcels[parcelLayer].activeParcel = activeParcel;
-        state.parcels[parcelLayer].activeAddress = activeAddress;
-        state.parcels[parcelLayer].activeMapreg = activeMapreg;
       },
       setActiveFeature(state, payload) {
         const { featureId, tableId } = payload || {};
@@ -161,6 +152,9 @@ function createStore(config) {
         const { featureId } = payload || {};
         const nextActiveFeature = { featureId };
         state.activeModal = nextActiveFeature;
+      },
+      setActiveModalFeature(state, payload) {
+        state.activeModalFeature = payload;
       },
       setImageOverlay(state, payload) {
         state.map.imageOverlay = payload;
