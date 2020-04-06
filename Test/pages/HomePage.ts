b45f8ccbbb866302bfb90070f7fe0f681603aeea
Anusha Errabelli <37459695+AnushaErrabelli@users.noreply.github.com>
2020-04-06 10:21:29 -0400

    adding Testcafe automation framework with two testscripts (#387)
    Adding automation script to Property data Explorer which Includes
    1. General search script
    2. Polygon search script

---
+++ Test/pages/HomePage.ts
@@ -0,0 +1,37 @@
+import { Selector } from "testcafe";
+import { buildingAddressData } from "../helpers/searchData";
+
+export default class HomePage {
+  pageTitle: Selector = Selector("[class='app-title']");
+  citylinkButton: Selector = Selector("a[href='https://www.phila.gov']");
+  howtouseButton: Selector = Selector(".app-footer:nth-child(4) .cell div")
+  closeHowtouseButton: Selector = Selector("svg[data-icon='times-circle']");
+  feedbackButton: Selector = Selector(".app-footer:nth-child(4) li:nth-child(3) > a");
+  searchMap: Selector = Selector("input[placeholder='Search the map']");
+  searchmapButton: Selector = Selector("button[name='pvm-search-control-button']");
+  table: Selector = Selector('table');
+  mailingLables: Selector = Selector("svg[data-icon='download']");
+  downloadCSV: Selector = Selector("svg[data-icon='envelope']");
+
+    // Verify page landing
+  public verifyPageFunctionality = async (t: TestController) => {
+    await t.expect(this.pageTitle.visible).ok();
+    await t.expect(this.feedbackButton.visible).ok();
+    await t.expect(this.citylinkButton.visible).ok();
+    await t.click(this.howtouseButton);
+    await t.click(this.closeHowtouseButton);
+    await t.typeText(this.searchMap,  buildingAddressData.address);
+    await t.click(this.searchmapButton);
+    await t.wait(2000);
+    await t.click(this.downloadCSV);
+    await t.click(this.mailingLables);
+    await t.wait(2000);
+    const tableValues = await this.table.innerText;
+    await t.expect(tableValues).contains(buildingAddressData.opaAddress)
+    var tablecount = await this.table.count;
+    for (var i=0; i < tablecount; i++)
+    await t.click(this.table.nth(i))
+    await t.expect(tableValues).contains(buildingAddressData.opaAddress);
+   
+  }
+}
---
+++ Test/tests/testHomePage.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import HomePage from "../pages/HomePage";
+
+fixture`home page verification`.page(`${process.env.TEST_URL}`);
+
+test("page landing verification", async (t: TestController) => {
+  const homePage = new HomePage();
+  await homePage.verifyPageFunctionality(t);
+});
