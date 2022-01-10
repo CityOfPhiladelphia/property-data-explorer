694126240f1db348be68368ee817a3c701b42d90
Andy Rothwell <Andy.Rothwell@phila.gov>
2022-01-10 15:35:56 -0500

    Revert "attempted to solve 500 admirals way issue"
    This reverts commit 6f240afec9c51d08aeaf41504b3f9a39b4811544.

--- src/data-sources/opa-assessment.js
+++ src/data-sources/opa-assessment.js
@@ -25,15 +25,7 @@ export default {
         // console.log('opa-assessment in if condo is running');
         // opa.push(state.geocode.data);
         let idNumber = state.parcels.pwd ? Number(state.parcels.pwd[0].properties.PARCELID) : state.geocode.data.properties.dor_parcel_id;
-        let unitKeys = Object.keys(state.condoUnits.units);
-        console.log('opa-assessment.js idNumber:', idNumber, 'unitKeys:', unitKeys);
-        // if (Object.keys(state.condoUnits.units)
         opa.push(state.condoUnits.units[idNumber][0]);
-        // if (unitKeys[0] !== '') {
-        //   opa.push(state.condoUnits.units[idNumber][0]);
-        // } else {
-        //   opa.push(state.condoUnits.units[''][0]);
-        // }
       }
       return opa;
 
