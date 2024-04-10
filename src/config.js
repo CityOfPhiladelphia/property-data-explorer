80f4fbc19b44ccbea7a8ba66702bddbef9a0b56f
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-04-10 09:25:25 -0400

    adds 2023 imagery

--- src/config.js
+++ src/config.js
@@ -188,6 +188,19 @@ let config = {
         type: 'raster',
       },
     },
+    imagery2023: {
+      source: {
+        tiles: [
+          'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_2023/MapServer/tile/{z}/{y}/{x}',
+        ],
+        type: 'raster',
+        tileSize: 256,
+      },
+      layer: {
+        id: 'imagery2023',
+        type: 'raster',
+      },
+    },
     imagery2022: {
       source: {
         tiles: [
