a58236517cbe6ca8fb997bc53de916e804f63909
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2026-03-04 15:37:04 -0500

    adds 2025 imagery

--- src/components/MapPanel.vue
+++ src/components/MapPanel.vue
@@ -978,7 +978,7 @@ export default {
   mounted() {
     // console.log('MapPanel mounted is running, DrawControl', DrawControl)
     const map = this.$store.state.map.map;
-    this.$store.commit('setImagery', 'imagery2024');
+    this.$store.commit('setImagery', 'imagery2025');
   },
   methods: {
     handleBufferButtonClick(e) {
